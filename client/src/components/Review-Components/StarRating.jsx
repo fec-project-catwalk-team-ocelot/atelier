74e9ee0d119fddd97d40cb9108d19c1d64be15cc
Jeff Liu <theycallmejeff@gmail.com>
2021-09-11 23:38:43 -0700

    Working on using context providers

--- client/src/components/Review-Components/StarRating.jsx
+++ client/src/components/Review-Components/StarRating.jsx
@@ -8,11 +8,11 @@ function StarRating({ productId }) {
   const getReviews = (id) => {
     axios.get(`/api/reviews?product_id=${id}`)
       .then(({ data }) => {
-        const averageReview = data.results.map((item) => item.rating)
+        const averageReview = data.results.map(item => item.rating)
           .reduce((prev, curr) => prev + curr);
         setAverage(averageReview / data.results.length);
       })
-      .catch((err) => {
+      .catch(err => {
         if (err) {
           console.log(err);
         }
@@ -24,7 +24,10 @@ function StarRating({ productId }) {
   }, []);
 
   return (
-    <StarRatings rating={average} starRatedColor="rgb(253,204,13)" numberOfStars={5} starDimension="30px" />
+    <div>
+      <StarRatings rating={average} starRatedColor="rgb(253,204,13)" numberOfStars={5} starDimension="30px" />
+    </div>
+
   );
 }
 
