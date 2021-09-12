061c88f804215b624fc0fa422e5d485d8272347c
Jeff Liu <theycallmejeff@gmail.com>
2021-09-11 18:29:07 -0700

    Updated App.js

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,16 +1,17 @@
 import React, { useState } from 'react';
 // import bootstrap from 'bootstrap';
-
+import axios from 'axios';
+import Navbar from './Navbar.jsx';
 import Overview from './Overview.jsx';
 import RelatedItems from './RelatedItems.jsx';
 import QuestionsAndAnswers from './QuestionsAndAnswers.jsx';
 import Reviews from './Reviews.jsx';
-import axios from 'axios';
 
 function App() {
   const [productId, setProductId] = useState(42366);
   return (
     <div>
+      <Navbar />
       <Overview productId={productId} />
       <RelatedItems productId={productId} setProductId={setProductId} />
       <QuestionsAndAnswers productId={productId} />
