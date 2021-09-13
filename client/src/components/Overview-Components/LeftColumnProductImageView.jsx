509ff69ec31b612e12a64c8ffcd237355f32c0e2
Jeff Liu <theycallmejeff@gmail.com>
2021-09-13 14:47:41 -0700

    Completed Zoom and pan feature

--- client/src/components/Overview-Components/LeftColumnProductImageView.jsx
+++ client/src/components/Overview-Components/LeftColumnProductImageView.jsx
@@ -1,11 +1,12 @@
 /* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 /* eslint-disable react/prop-types */
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { BiFullscreen } from 'react-icons/bi';
 
 function LeftColumnProductImageView({ selectedStyle, fullscreenToggle, setFullscreenToggle }) {
   let renderMainImages;
+  const [zoom, setZoom] = useState(false);
 
   function handleFullscreen() {
     setFullscreenToggle(!fullscreenToggle);
@@ -18,8 +19,10 @@ function LeftColumnProductImageView({ selectedStyle, fullscreenToggle, setFullsc
         idx={idx}
         photoUrl={photo.url}
         altText={selectedStyle.name}
-        handleFullscreen={handleFullscreen}
+        setFullscreenToggle={setFullscreenToggle}
         fullscreenToggle={fullscreenToggle}
+        zoom={zoom}
+        setZoom={setZoom}
       />
     ));
   } else {
@@ -41,29 +44,28 @@ function LeftColumnProductImageView({ selectedStyle, fullscreenToggle, setFullsc
   }
 
   return (
-    // TODO: click image in fullscreen view will zoom 2.5x
-    // TODO: in zoom mode, image should pan with mouse
-    // TODO: mouse icon should be '-',no arrows or indicators
     // FIXME: on expanded view, thumbnails become icons
-    <div className={`${fullscreenToggle ? 'col-lg-12 showFullscreen' : 'col-lg-8'}`}>
-      <div id="mainImage" className="carousel slide carousel-fade bg-light" data-interval="false" data-keyboard="true">
-        <div className="carousel-inner">
+    <div className={`${fullscreenToggle ? 'col-lg-12 showFullscreen h-100 g-0' : 'col-lg-8'}`}>
+      <div id="mainImage" className="carousel slide carousel-fade bg-light" data-bs-interval="false">
+        <div id="mainImageInner" className="carousel-inner">
           {renderMainImages}
         </div>
-        <button type="button" className="fullscreen-icon" onClick={handleFullscreen}>
+        <button type="button" className={`fullscreen-icon ${zoom ? 'd-none' : ''}`} onClick={handleFullscreen}>
           <BiFullscreen size="2em" />
         </button>
-        <div className="carousel-indicators d-flex flex-column justify-content-start">
-          {renderMainThumbnails}
+        <div className={`${zoom ? 'd-none' : ''}`}>
+          <div className="carousel-indicators d-flex flex-column justify-content-start ">
+            {renderMainThumbnails}
+          </div>
+          <button className="carousel-control-prev" type="button" data-bs-target="#mainImage" data-bs-slide="prev">
+            <span className="carousel-control-prev-icon" aria-hidden="true" />
+            <span className="visually-hidden">Previous</span>
+          </button>
+          <button className="carousel-control-next" type="button" data-bs-target="#mainImage" data-bs-slide="next">
+            <span className="carousel-control-next-icon" aria-hidden="true" />
+            <span className="visually-hidden">Next</span>
+          </button>
         </div>
-        <button className="carousel-control-prev" type="button" data-bs-target="#mainImage" data-bs-slide="prev">
-          <span className="carousel-control-prev-icon" aria-hidden="true" />
-          <span className="visually-hidden">Previous</span>
-        </button>
-        <button className="carousel-control-next" type="button" data-bs-target="#mainImage" data-bs-slide="next">
-          <span className="carousel-control-next-icon" aria-hidden="true" />
-          <span className="visually-hidden">Next</span>
-        </button>
       </div>
     </div>
   );
@@ -71,16 +73,33 @@ function LeftColumnProductImageView({ selectedStyle, fullscreenToggle, setFullsc
 
 // Helper Render Functions
 
-function MainImages({ photoUrl, altText, idx, handleFullscreen, fullscreenToggle }) {
-  const [zoom, setZoom] = useState(false);
-  const [position, setPosition] = useState([]);
-  function handleZoom() {
+function MainImages(props) {
+  const {
+    photoUrl, altText, idx, setFullscreenToggle, fullscreenToggle, setZoom, zoom,
+  } = props;
+
+  function handleFullscreen() {
+    setFullscreenToggle(!fullscreenToggle);
+  }
+
+  function handleZoom(e) {
     setZoom(!zoom);
+    if (fullscreenToggle) {
+      e.target.style.transform = '';
+    }
   }
 
   function getMouseLocation(e) {
-    // need to get the mouse location on the image hover
-    setPosition([Number(e.clientX), Number(e.clientY)]);
+    const posX = e.clientX; // 1 - 1680
+    const posY = e.clientY - 79; // 1 - 835, account for navbar size
+    const winX = e.target.offsetWidth; // 1692
+    const winY = e.target.offsetHeight; // 834
+    const picX = e.target.naturalWidth * 2.5; // 668
+    const picY = e.target.naturalHeight * 2.5; // 1002
+
+    const renderY = ((posY / winY) - 0.5) * (picY / 2);
+    const renderX = ((posX / winX) - 0.5) * (picX / 2);
+    e.target.style.transform = `translate(${renderX}px, ${renderY}px) scale(2.5)`;
   }
 
   return (
