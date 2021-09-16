1fbc303b84dd918d8df920f05b258a33957d4554
Yingchen <pepperbai1114@gmail.com>
2021-09-15 18:56:24 -0700

    add answerer name

--- client/src/components/Q&A-Components/Answer.jsx
+++ client/src/components/Q&A-Components/Answer.jsx
@@ -1,12 +1,20 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 
-const Answer = ({ answer }) => (
-  <div className="a-entry">
+const Answer = ({ answer }) => {
+  const [helpful, setHelpful] = useState(answer.helpfulness);
+  const [voted, setVoted] = useState(false);
+  const [reported, setReported] = useState(false);
+
+
+
+  return (
+    <div className="a-entry">
     <span>
       {`A: ${answer.body}`}
     </span>
   </div>
-);
+  );
+};
 
 export default Answer;
