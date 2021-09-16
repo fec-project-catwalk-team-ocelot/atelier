2b510d6d76f70002583e02a1a81747d51aaeff8a
Yingchen <pepperbai1114@gmail.com>
2021-09-16 13:53:00 -0700

    add seller and answername

--- client/src/components/Q&A-Components/Answer.jsx
+++ client/src/components/Q&A-Components/Answer.jsx
@@ -6,14 +6,43 @@ const Answer = ({ answer }) => {
   const [voted, setVoted] = useState(false);
   const [reported, setReported] = useState(false);
 
+  const handleHelpClick = () => {
+    if (!voted) {
+      setVoted((vote) => !vote);
+      setHelpful((helped) => helped + 1);
+      axios.put(
+        `/api/qa/answers/${answer.id}/helpful`,
+        {
+          helpfulness: helpful,
+        },
+      )
+        .then(() => {
 
+        })
+        .catch((res, err) => {
+          res.end('could not make answer more helpful', err);
+        });
+    }
+  };
 
   return (
     <div className="a-entry">
-    <span>
-      {`A: ${answer.body}`}
-    </span>
-  </div>
+      <div className="a-bullet">
+        {`A: ${answer.body}`}
+      </div>
+      <div className="Ahelp-report">
+        <span>By:</span>
+        <span className="seller">
+          {answer.answerer_name === 'Seller' ? `${answer.answerer_name}, ` : null}
+        </span>
+        <span>
+          {answer.answerer_name !== 'Seller' ? `${answer.answerer_name}, ` : null}
+        </span>
+        {` ${new Date(answer.date).toLocaleDateString(
+          undefined, { year: 'numeric', month: 'long', day: 'numeric' },
+        )}`}
+      </div>
+    </div>
   );
 };
 
