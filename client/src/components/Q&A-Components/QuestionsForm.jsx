5197b432b1eda114de408dd142a79ea4de8d5cef
Yingchen <pepperbai1114@gmail.com>
2021-09-17 15:26:03 -0700

    change icon style

--- client/src/components/Q&A-Components/QuestionsForm.jsx
+++ client/src/components/Q&A-Components/QuestionsForm.jsx
@@ -89,7 +89,7 @@ const QuestionForm = ({ productId }) => {
               </div>
               <div className="modal-footer">
                 <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                <button type="submit" className="btn btn-primary" data-bs-dismiss="modal">Submit Question</button>
+                <button type="submit" className="btn btn-outline-dark" data-bs-dismiss="modal">Submit Question</button>
               </div>
             </form>
           </div>
