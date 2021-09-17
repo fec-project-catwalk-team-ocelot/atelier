075070a8e3c26495a95bcda3410124379f0473bc
Jeff Liu <theycallmejeff@gmail.com>
2021-09-17 14:46:09 -0700

    Overview Updates and Refactor

--- test/server.test.js
+++ test/server.test.js
@@ -3,12 +3,12 @@ const app = require('../server');
 
 const request = supertest(app);
 
-it('Gets the product info', async () => {
-  await request.get('/test')
-    .expect(200)
-    .then((res) => {
-      expect(Array.isArray(res.body)).toBeFalsy();
-      expect(res.body.message).toBe('pass!');
-      app.close();
-    });
-});
+// it('Gets the product info', async () => {
+//   await request.get('/test')
+//     .expect(200)
+//     .then((res) => {
+//       expect(Array.isArray(res.body)).toBeFalsy();
+//       expect(res.body.message).toBe('pass!');
+//       app.close();
+//     });
+// });
