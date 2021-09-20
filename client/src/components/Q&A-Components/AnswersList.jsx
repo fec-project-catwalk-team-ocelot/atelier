29a5308f806785a62d0558ef3fe3feb6b26bdcf8
Yingchen <pepperbai1114@gmail.com>
2021-09-20 11:27:13 -0700

    add scroll

--- client/src/components/Q&A-Components/AnswersList.jsx
+++ client/src/components/Q&A-Components/AnswersList.jsx
@@ -2,14 +2,39 @@ import React, { useState } from 'react';
 import Answer from './Answer.jsx';
 
 const AnswersList = ({ answers }) => {
-  answers.sort((a, b) => b.helpfulness - a.helpfulness);
   const [moreAnswers, showMoreAnswers] = useState(false);
+  answers.sort((a, b) => b.helpfulness - a.helpfulness);
+
+  // const filteredAnswers = [];
+  // for (let i = 0; i < answers.length; i++) {
+  //   if (Object.keys(answers[i].answers).length !== 0) {
+  //     filteredQuestions.push(questions[i]);
+  //   }
+  // }
 
   const handleMoreAnswers = () => {
     showMoreAnswers((more) => !more);
   };
 
-
+  if (moreAnswers) {
+    return (
+      <>
+        <ul className="answer-list">
+          {answers.map((answer) => (
+            <div key={answer.answer_id}>
+              <Answer answer={answer} key={answer.answer_id} />
+            </div>
+          ))}
+        </ul>
+        <button
+          className="more-answers-button btn btn-outline-dark"
+          onClick={handleMoreAnswers}
+      >
+        HIDE ANSWERS
+      </button>
+      </>
+    );
+  }
   return (
     <div>
       <ul className="answer-list">
@@ -22,7 +47,7 @@ const AnswersList = ({ answers }) => {
         ))}
       </ul>
       <button
-        className="buttons more-answers-button"
+        className="more-answers-button btn btn-outline-dark"
         onClick={handleMoreAnswers}
       >
         LOAD MORE ANSWERS
